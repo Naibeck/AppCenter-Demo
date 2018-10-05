9b651e076c1a83a18f46d7853e338fa55ff09ad5
Naibeck <kg-diseno@outlook.es>
2018-10-04 21:10:30 -0600

    Add events and more tests

--- app/src/main/java/com/naibeck/poc/appcenter/MainActivity.kt
+++ app/src/main/java/com/naibeck/poc/appcenter/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.naibeck.poc.appcenter
 
+import android.content.res.Configuration
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.widget.Button
@@ -16,11 +17,22 @@ class MainActivity : AppCompatActivity() {
         initAppCenter()
 
         val button = findViewById<Button>(R.id.button)
-        button.setOnClickListener { button.text = getString(R.string.pressed_feedback) }
+        button.setOnClickListener {
+            Analytics.trackEvent("CTA clicked")
+            button.text = getString(R.string.pressed_feedback)
+        }
+    }
+
+    override fun onConfigurationChanged(newConfig: Configuration?) {
+        super.onConfigurationChanged(newConfig)
+        when (newConfig?.orientation) {
+            Configuration.ORIENTATION_LANDSCAPE -> Analytics.trackEvent("Landscape rotation")
+            Configuration.ORIENTATION_PORTRAIT -> Analytics.trackEvent("Portrait rotation")
+        }
     }
 
     private fun initAppCenter() {
         AppCenter.start(application, "80c08496-d4c0-46d9-8ea8-4f5ffb569571",
-                Analytics::class.java, Crashes::class.java)
+                Analytics::class.java, Crashes::class.java, Analytics::class.java)
     }
 }
--- app/src/test/java/com/naibeck/poc/appcenter/MainActivityTest.kt
+++ app/src/test/java/com/naibeck/poc/appcenter/MainActivityTest.kt
@@ -1,11 +1,14 @@
 package com.naibeck.poc.appcenter
 
+import android.content.res.Configuration
 import android.widget.Button
 import org.junit.Assert
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.robolectric.Robolectric
 import org.robolectric.RobolectricTestRunner
+import org.robolectric.RuntimeEnvironment
+import org.robolectric.annotation.Config
 
 @RunWith(RobolectricTestRunner::class)
 class MainActivityTest {
@@ -24,4 +27,33 @@ class MainActivityTest {
         Assert.assertEquals("Button text should shown: Pressed!", expectedFeedback, button.text.toString())
     }
 
+    @Test @Config(qualifiers = "port")
+    fun `Detect configuration change from portrait to landscape`() {
+        // Setup
+        val controller = Robolectric.buildActivity(MainActivity::class.java)
+
+        // Exercise
+        RuntimeEnvironment.setQualifiers("land")
+        controller.configurationChange()
+
+        // Validation
+        val expectedOrientation = Configuration.ORIENTATION_LANDSCAPE
+        Assert.assertEquals("Configuration should be landscape", expectedOrientation,
+                controller.get().resources.configuration.orientation)
+    }
+
+    @Test @Config(qualifiers = "land")
+    fun `Detect configuration change from landscape to portrait`() {
+        // Setup
+        val controller = Robolectric.buildActivity(MainActivity::class.java)
+
+        // Exercise
+        RuntimeEnvironment.setQualifiers("port")
+        controller.configurationChange()
+
+        // Validation
+        val expectedOrientation = Configuration.ORIENTATION_PORTRAIT
+        Assert.assertEquals("Configuration should be landscape", expectedOrientation,
+                controller.get().resources.configuration.orientation)
+    }
 }
